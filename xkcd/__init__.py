152d898793eeea6c052ae55a77e58384bd0b6789
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-10-01 15:17:04 +0100

    Update __init__.py

--- xkcd/__init__.py
+++ xkcd/__init__.py
@@ -1,5 +1,5 @@
 from .xkcd import XKCD
 
-async def setup(bot):
-    n = XKCD(bot)  # Pass the 'bot' instance to the XKCD class
-    bot.add_cog(n)
+async def setup(bot):  # Async setup for Redbot/discord.py 2.0
+    n = XKCD(bot)  # Pass the bot instance to the XKCD cog
+    await bot.add_cog(n)  # Use 'await' with add_cog for asynchronous behavior
