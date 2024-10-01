3a9723e10bed1679b03d0f482638b9856c557107
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-10-01 15:12:30 +0100

    Update __init__.py

--- xkcd/__init__.py
+++ xkcd/__init__.py
@@ -1,6 +1,5 @@
-from redbot.core import commands
 from .xkcd import XKCD
 
 async def setup(bot):
-    n = XKCD(bot)
-    await bot.add_cog(n)
+    n = XKCD(bot)  # Pass the 'bot' instance to the XKCD class
+    bot.add_cog(n)
